8a4717c6d476198067155c1d88b7e4716c896463
De4th-Li0n <73570727+De4th-Li0n@users.noreply.github.com>
2022-05-15 11:31:24 +0700

    Update 12
    -Added Mini Map
    -Added Track Selection
    -Added New Track
    -Added Car Spotlight
    -Added Image to Button to show each track

--- Assets/Scripts/Core/MainMenu.cs
+++ Assets/Scripts/Core/MainMenu.cs
@@ -12,4 +12,10 @@ public class MainMenu : MonoBehaviour
     {
         Application.Quit();
     }
+    public void Track01 () {
+		SceneManager.LoadScene (1);
+	}
+    public void Track02 () {
+		SceneManager.LoadScene (2);
+	}
 }
