c4af39c9391e36d468f6c89d5a888152ef9f58d8
De4th-Li0n <73570727+De4th-Li0n@users.noreply.github.com>
2022-05-16 10:58:36 +0700

    Update 13
    -Added Car & Track Selection
    -Allow to Choose Car And Track
    -Begining to make Track 2 for playing

---
+++ Assets/Scripts/Core/CarChoice.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarChoice : MonoBehaviour
+{
+    //1=Red, 2=Pale, 3=Yellow, 4=Pink
+    public GameObject RedCar;
+    public GameObject PaleCar;
+    public GameObject YellowCar;
+    public GameObject PinkCar;
+    public int CarImport;
+
+    void Start()
+    {
+        CarImport = GlobalCar.CarType;
+        if (CarImport == 1)
+        {
+            RedCar.SetActive(true);
+        }
+
+        if (CarImport == 2)
+        {
+            PaleCar.SetActive(true);
+        }
+        if (CarImport == 3)
+        {
+            YellowCar.SetActive(true);
+        }
+        if (CarImport == 4)
+        {
+            PinkCar.SetActive(true);
+        }
+    }
+}
+
---
+++ Assets/Scripts/Core/GlobalCar.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalCar : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static int CarType; //1=Red, 2=Pale, 3=Yellow, 4=Pink
+    public GameObject TrackWindow;
+
+    public void RedCar()
+    {
+        CarType = 1;
+        TrackWindow.SetActive(true);
+    }
+
+    public void PaleCar()
+    {
+        CarType = 2;
+        TrackWindow.SetActive(true);
+    }
+    public void YellowCar()
+    {
+        CarType = 3;
+        TrackWindow.SetActive(true);
+    }
+    public void PinkCar()
+    {
+        CarType = 4;
+        TrackWindow.SetActive(true);
+    }
+}
