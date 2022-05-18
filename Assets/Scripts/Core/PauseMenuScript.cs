eb1ae037a6ef4c16f0bd757e78512eabd84ca8fd
De4th-Li0n <73570727+De4th-Li0n@users.noreply.github.com>
2022-05-18 17:43:18 +0700

    Update 14
    -Added Pause Menu
    -Added Roads for track 2
    -Added Bounderies to track 2
    -Fix Some other bugs related to track 2
    -Finished Track 2

---
+++ Assets/Scripts/Core/PauseMenuScript.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuScript : MonoBehaviour
+{
+    [SerializeField] GameObject pauseMenu;    
+
+    public static bool GameisPause = false;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameisPause)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Pause()
+    {        
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        GameisPause = true;
+        AudioListener.volume = 0f;
+    }
+
+    public void Resume()
+    {        
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        GameisPause = false;
+        AudioListener.volume = 1f;
+    }
+    public void Home(int sceneID)
+    {        
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(sceneID);
+        AudioListener.volume = 1f;
+    }
+    public void QuitGame()
+    {        
+        Application.Quit();
+        AudioListener.volume = 1f;
+    }
+}
